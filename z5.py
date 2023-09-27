9116bf7fd73d02bcb0ffd87f0878a370336e1a13
root <rit.2002014@gmail.com>
2023-09-27 16:33:14 +0700

    3 Lesson

--- z5.py
+++ z5.py
@@ -7,18 +7,15 @@ list_times = []
 np_times = [] 
 element_counts = [] 
    
-for i in range(10, 500000, 50000): 
+for i in range(10, 5000000, 150000): 
 	element_counts.append(i) 
-	#print("1")
 	big_np_array = np.random.sample(i) 
 	start = time.time() 
 	np.sort(big_np_array) 
 	end = time.time() - start 
 	np_times.append(end) 
 	big_lists_array = []
-	#print("2") 
 	for j in range(i):
-		#print("3") 
 		big_lists_array.append(random.random()) 
 	start = time.time() 
 	big_lists_array.sort() 
